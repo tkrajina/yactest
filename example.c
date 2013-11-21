6270f3bafe04671378da0f77e5c3f78e5fcea1c0
Tomo Krajina <tkrajina@gmail.com>
2013-11-21 07:09:32 +0100

    Example with blocks

--- example.c
+++ example.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "ctest.h"
 
@@ -6,16 +7,23 @@ CTEST_INIT();
     // Inline test:
     CTEST_INLINE(test1234, {
         CTEST_ASSERT(1 + 2 == 3);
-        CTEST_OK();
     });
 
-    // New suite:
-    CTEST_SUITE(suite2);
+    // Start new suite:
+    CTEST_SUITE(more_tests);
 
     // Function test:
     int test_function_alone() {
         CTEST_ASSERT(3 * 4 == 12);
-        CTEST_OK();
+        // Test functions must return true:
+        return 1;
     }
     CTEST_FUNCTION(test_with_function, test_function_alone);
+
+    // Test block of code, this is the preferred way;
+    CTEST_BLOCK_START();
+        int a = 1 + 2;
+        int b = 5 - 2;
+        CTEST_ASSERT(a == b);
+    CTEST_BLOCK_FINISH(block_test);
 CTEST_FINISH()
